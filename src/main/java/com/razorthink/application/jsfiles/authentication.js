f8105773c50eb6937fe2083cff456ea52c6bcea3
RAKESHNM <rakesh@razorthink.net>
2017-03-09 11:36:37 +0530

    Added checkout html page changes

--- src/main/java/com/razorthink/application/jsfiles/authentication.js
+++ src/main/java/com/razorthink/application/jsfiles/authentication.js
@@ -21,7 +21,7 @@
     method: 'POST',
      success:function(data2){
      if(data2 == 'Success'){
-     location.href = "../htmlfiles/sample.html";
+     location.href = "../htmlfiles/CheckoutService.html";
      }
      },
 
