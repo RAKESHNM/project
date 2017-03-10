ab1f9fe4d443bb286124b14de0e73e842246dad6
antolivish <anto.livish@razorthink.net>
2017-03-10 20:32:34 +0530

    UI services completed

--- src/main/resources/jsfiles/CommandTest.js
+++ src/main/resources/jsfiles/CommandTest.js
@@ -1,49 +1,46 @@
+var data = {};
 $(document).ready(function(){
     var header = "header";
+
    var options = ["Commit Details","Project Summary","List all methods", "List all methods having lines greater than n", "List all methods without javadocs"];
-
- 	$('#selectCommand').empty();
-
-	  var output = [];
-	  options.forEach(function(key){
-	   output.push('<option class="option_click">'+ key +'</option>');
-	  })
-	  console.log(output)  ;
-	  $('#selectCommand').html(output.join(''));
-	  $(".module-wrapper").hide();
-   	  $(".lines-wrapper").hide();
-	  $("#selectCommand").on("click", function(value){
-	  	if((document.getElementById("selectCommand").value == options[0])||(document.getElementById("selectCommand").value == options[1])){
-   				 $(".module-wrapper").hide();
-   				 $(".lines-wrapper").hide();
-    			}
-    	else if(document.getElementById("selectCommand").value == options[3]){
-   				 $(".module-wrapper").show();
-   				 $(".lines-wrapper").show();
-    			}
+   $('#selectCommand').empty();
+     var output = [];
+     options.forEach(function(key){
+      output.push('<option class="option_click">'+ key +'</option>');
+     })
+     console.log(output)  ;
+     $('#selectCommand').html(output.join(''));
+     $(".module-wrapper").hide();
+         $(".lines-wrapper").hide();
+     $("#selectCommand").on("click", function(value){
+      if((document.getElementById("selectCommand").value == options[0])||(document.getElementById("selectCommand").value == options[1])){
+                 $(".module-wrapper").hide();
+                 $(".lines-wrapper").hide();
+             }
+       else if(document.getElementById("selectCommand").value == options[3]){
+                 $(".module-wrapper").show();
+                 $(".lines-wrapper").show();
+             }
         else {
-        		 $(".module-wrapper").show();
-    		 	 $(".lines-wrapper").hide();
-
-    		 }
-		});
-
+               $(".module-wrapper").show();
+              $(".lines-wrapper").hide();
+           }
+      });
        $(".button").click(function(){
            getCommandService();
             $(".popup").addClass("showClass");
                 $(".popup").show();
        })
-
        $(".closeIcon").click(function(){
             $(".popup").hide();
-            $(".method").empty();
-            $(".linemethod").empty();
+            $(".wrapper").empty();
+            $(".wrapper").append('<div class="linemethod"></div><div id="summary"></div><a href="" class="method"></a>');
+//            $(".method").empty();
+//            $(".linemethod").empty();
         })
-
        $(".message").append("Message To Be Displayed");
 //    getCommandService();
 });
-
 function multiplyNode(node, count, deep, obj) {
         for (var i = 0, copy; i < obj.length; i++) {
             copy = node.cloneNode(deep);
@@ -51,14 +48,10 @@ function multiplyNode(node, count, deep, obj) {
             console.log(copy);
             node.parentNode.insertBefore(copy, node)
             console.log(node);
-
         }
 //        $(".method").append(obj);
 //        $(".method").append(" ");
-
     }
-
-
 function getCommandService(){
 var result;
 var errorres = [];
@@ -68,9 +61,8 @@ var errorres = [];
                 data.subModule=document.getElementById("module").value;
                 data.file=document.getElementById("file").value;
                 data.noOfLines=document.getElementById("number").value;
-
           $.ajax({
-            url:"http://localhost:8080/inputs",
+            url:"/rest/inputs",
             type: 'POST',
              crossDomain : true,
               headers: {
@@ -100,7 +92,6 @@ var errorres = [];
 //                multiplyNode(document.querySelector(".linemethod"), (key.length), true, key);
                 $(".method").append("<li>"+ key + "</li>");
 //                $(".method").empty();
-
                 }
                 }
                 else{
@@ -109,7 +100,6 @@ var errorres = [];
 //                for(var i=0; i<result.object.length;i++){
 //                $(".method").append(result.object);
 //                }
-
             },
             error: function(errorres){
                 console.log(errorres);
