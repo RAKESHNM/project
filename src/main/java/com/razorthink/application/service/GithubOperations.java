45a4266c594fc804e803aa89ced088d3cdcae00c
RAKESHNM <rakesh@razorthink.net>
2017-03-14 18:13:57 +0530

    Updated with master

--- src/main/java/com/razorthink/application/service/GithubOperations.java
+++ src/main/java/com/razorthink/application/service/GithubOperations.java
@@ -179,9 +179,9 @@ public class GithubOperations {
            System.out.println(commit.getFullMessage());
             commitList.add(commit.getFullMessage());
             System.out.println(commit.getFullMessage().length());
-            commitList.add(commit.getAuthorIdent().getName() + " committed on " + date.toString() + "\n");
+            commitList.add(commit.getAuthorIdent().getName());
+            commitList.add(date.toString());
 //           commitList.add(commit.getAuthorIdent().getName() + " committed on " + date.toString() + "\n");
-           commitList.add("\n");
 //            commitList.add(date.toString());
 
         }
